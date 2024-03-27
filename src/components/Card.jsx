8508a6bece99f191aaf216558318238440f1b332
Devfrank94 <francescomurro94@gmail.com>
2024-03-27 23:15:19 +0100

    fix card dimension

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable react/prop-types */
 
-function Card({ title, imgURL }) {
-  
-  const description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum non aliquam recusandae mollitia adipisci sunt obcaecati."
+function Card({ title, imgURL, description }) {
+
 
 
   return(
       <div className="card flex-auto mx-2 w-60 h-max text-center rounded-lg bg-zinc-900 shadow-2xl">
         <img className="object-cover h-52 rounded w-full" src={imgURL} alt={title} />
-        <div className="p-4 h-max">
+        <div className="p-4 h-64 overflow-auto">
           <h2 className="text-2xl text-white font-bold">{title}</h2>
           <p className="text-gray-500/75 text-wrap sm:text-balance">{description}</p>
         </div>
