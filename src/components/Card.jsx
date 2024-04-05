9f40fe309c695fa0a07f339b11ebe5f5e15b0c19
Devfrank94 <francescomurro94@gmail.com>
2024-04-05 13:01:29 +0200

    add condition visited and fix breakpoint

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,25 @@
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline'
 /* eslint-disable react/prop-types */
 
-function Card({ title, imgURL, description }) {
+function Card({ title, imgURL, isVisited, description }) { 
 
-
+  // Condizione che mostra la spunta verde o la x rossa  a seconda se il luogo è stato visitato o meno.
+  const visitedLabel = (
+    <span>
+      {isVisited ? <CheckCircleIcon className="inline h-6 w-6 m-3 text-green-700"/> : <XCircleIcon className="inline h-6 w-6 m-3 text-red-700"/>}
+      <p className='text-white font-semibold'> {isVisited ? 'Visitata' : 'Non visitata'}</p>
+    </span>
+  );
 
   return(
-      <div className="card flex-auto mx-2 w-60 h-max text-center rounded-lg bg-zinc-900 shadow-2xl">
+      <div className="card cursor-pointer hover:scale-110 transition delay-150 duration-300 ease-in-out flex-none mx-2 w-60 h-max text-center rounded-lg bg-zinc-900 shadow-2xl">
         <img className="object-cover h-52 rounded w-full" src={imgURL} alt={title} />
         <div className="p-4 h-64 overflow-auto">
           <h2 className="text-2xl text-white font-bold">{title}</h2>
           <p className="text-gray-500/75 text-wrap sm:text-balance">{description}</p>
+          <div className="flex justify-center my-5">
+            <span>{visitedLabel}</span>
+          </div>
         </div>
       </div>
     
