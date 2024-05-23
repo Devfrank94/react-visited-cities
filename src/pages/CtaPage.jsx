98e3b6f88fec99b95919b3bbfb7d420436708ea7
Devfrank94 <francescomurro94@gmail.com>
2024-05-23 19:35:03 +0200

    add cta page component

---
+++ src/pages/CtaPage.jsx
@@ -0,0 +1,14 @@
+import Cta from '../components/Cta';
+import Navbar from '../components/Navbar'
+
+
+function CtaPage() {
+  return (
+      <div className="min-h-screen max-h-full bg-gradient-to-t from-gray-400 to-gray-800">
+      <Navbar/>
+      <Cta/>
+    </div>
+  )
+}
+
+export default CtaPage;
