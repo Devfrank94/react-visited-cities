c3c3b3809feda0754f75149677500ff5827a00ce
Devfrank94 <francescomurro94@gmail.com>
2024-05-23 19:33:44 +0200

    add home page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,15 @@
-import { useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar.jsx';
-import Card from './components/Card.jsx';
-import CardForm from './components/CardForm.jsx';
-import Select from './components/Select.jsx';
-import { useSelector } from 'react-redux';
-
+import Home from './pages/Home.jsx';
 
 
 function App() { 
 
-const cities = useSelector((state) => state.cities.value);
-
-const [filterValue, setSelectedValue ] = useState("all")
-
-const handleSelectChange = (value) => {
-  setSelectedValue(value);
-  console.log(value);
-};
-
   return (
-    <>
+    <div className="min-h-screen max-h-full bg-gradient-to-t from-gray-400 to-gray-800">
       <Navbar/>
-      <div className=" min-h-screen max-h-full bg-gradient-to-t from-gray-400 to-gray-800">
-        <CardForm/>
-        <Select onSelectChange={handleSelectChange}/>
-        <div className="py-10 flex justify-center flex-wrap min-[320px]:gap-6 sm:gap-6 md:gap-6">
-          {cities
-          .filter((city) => {
-            if (filterValue === "isVisited") {
-              return city.isVisited === true;
-            } else if (filterValue === "!isVisited") {
-              return city.isVisited === false;
-            } else {
-              return  cities;
-            } 
-          })
-          .map((city) => (
-                <Card 
-                key={city.id}
-                title={city.title}
-                imgURL={city.imgURL}
-                description={city.description}
-                isVisited={city.isVisited}
-                />
-            ))}
-        </div>
-      </div>
-    </>
+      <Home/>    
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Cards from "../components/Cards";
+
+function Home() {
+  return (
+    <div>
+      <Cards/>
+    </div>
+  )
+}
+
+export default Home;
