ef14d382556be5f8e69f824f233017f5eae6d5f8
Devfrank94 <francescomurro94@gmail.com>
2024-05-27 01:29:58 +0200

    add validation in form

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -53,27 +53,27 @@ function CardForm() {
     <div className="pt-5 grid place-content-center">
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-3 w-80 my-5 p-5 bg-gray-700 rounded-md shadow-lg">
-          <label htmlFor="imgURL" className="block text-sm font-medium leading-3 text-white">
+          <label htmlFor="imgURL" className="block text-sm font-medium leading-3 text-white after:content-['*'] after:ml-0.5 after:text-red-500">
             Immagine Card
           </label>
-            <input name="imgURL" type="text" value={formData.imgURL} onChange={handleInputChange} placeholder="Inserisci URL immagine" className="block rounded-md px-3 py-2 text-center text-sm font-semibold required:border-red-500 valid:border-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"></input>
+            <input name="imgURL" type="text" value={formData.imgURL} onChange={handleInputChange} placeholder="Inserisci URL immagine" className="block rounded-md px-3 py-2 text-center text-sm font-semibold  required:outline-red-500 valid:outline-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" minLength={10} required></input>
 
-          <label htmlFor="title" className="block text-sm font-medium leading-3 text-white">
+          <label htmlFor="title" className="block text-sm font-medium leading-3 text-white after:content-['*'] after:ml-0.5 after:text-red-500">
             Città
           </label>
-            <input name="title" type="text" value={formData.title} onChange={handleInputChange} placeholder="Inserisci nome Città" className="block rounded-md px-3 py-2 text-center text-sm font-semibold required:border-red-500 valid:border-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"></input>
+            <input name="title" type="text" value={formData.title} onChange={handleInputChange} placeholder="Inserisci nome Città" className="block rounded-md px-3 py-2 text-center text-sm font-semibold required:outline-red-500 valid:outline-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" minLength={2} maxLength={15} required></input>
 
-          <label htmlFor="description" className="block text-sm font-medium leading-3 text-white">
+          <label htmlFor="description" className="block text-sm font-medium leading-3 text-white after:content-['*'] after:ml-0.5 after:text-red-500">
             Descrizione
           </label>
-            <textarea name="description" value={formData.description} onChange={handleInputChange} placeholder="Inserisci descrizione..." className="block rounded-md px-3 py-2 text-center min-h-3.5 text-sm font-semibold required:border-red-500 valid:border-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"></textarea>
+            <textarea name="description" value={formData.description} onChange={handleInputChange} placeholder="Inserisci descrizione..." className="block rounded-md px-3 py-2 text-center min-h-3.5 text-sm font-semibold required:outline-red-500 valid:outline-green-500 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" minLength={20} maxLength={50} required></textarea>
 
           <label htmlFor="isVisited" className="block text-sm font-medium leading-3 text-white">
             Visitata?
           </label>
-            <input type="checkbox" name="isVisited" checked={formData.isVisited} onChange={handleInputChange} className="block required:border-red-500 valid:border-green-500 h-5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"></input>
+            <input type="checkbox" name="isVisited" checked={formData.isVisited} onChange={handleInputChange} className="block h-5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"></input>
         
-            <button type="submit" className="mt-3 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >Aggiungi Card</button>
+            <button type="submit" className="mt-3 block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" >Aggiungi Card</button>
         </div>
       </form>
     </div>
